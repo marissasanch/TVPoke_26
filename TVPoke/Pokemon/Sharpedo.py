7dac1a1513d1a0179636d9d8042d74a6657b6d60
benny42boo <bennettjohnston7610@gmail.com>
2026-03-06 11:15:37 -0700

    fixed sharp

--- TVPoke/Pokemon/Sharpedo.py
+++ TVPoke/Pokemon/Sharpedo.py
@@ -9,4 +9,4 @@ class Sharpedo(Water):
             Move("Hyper Beam", "NORMAL", 150),
             Move("Skull Bash", "NORMAL", 130)
         ]
-        super().__init__("Sharpedo", 70, moves, "./TVPoke/Pokemon/imgs/sharpedo.png")
+        super().__init__("Sharpedo", 70, moves, "./TVPoke/Pokemon/imgs/Sharpedo.png")
